d967dfb5bce9b5b8642b56ddda0b2f4e2fea5cf7
Keion-Larsen <keion.larsen@my.jcu.edu.au>
2019-03-22 10:32:33 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -3,3 +3,12 @@
 Keion Larsen
 
 '''
+
+enteredPass = str(input("Entered Password: "))
+if len(enteredPass) < 2:
+    enteredPass = str(input("Please Enter Valid Password: "))
+
+length = len(enteredPass)
+for i in range(length):
+    asterisks = '*'
+    print(asterisks.rstrip("/n"))
